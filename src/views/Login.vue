dd181502df810d807a504246f706b77628fbe5b5
barnacker <samuel.tremblay@bewave.io>
2018-12-22 11:55:00 -0500

    Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,11 @@
 <template>
-  <span>Login</span>
+  <v-container fluid fill-height>
+    <v-layout justify-center align-center>
+      <v-flex text-xs-center>
+        <span>Login</span>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
